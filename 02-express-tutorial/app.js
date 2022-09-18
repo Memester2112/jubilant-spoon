5317a8723f93894e97f7369b5e930161bcc32c1c
2efb7c0586666e0570786fb9594a1329a46582b6 <aryendrasingh1525@gmail.com>
2022-09-18 09:46:40 +0530

    Middlewares

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,49 +1,27 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./logger');
+const authorize = require('./authorize');
+const morgan = require('morgan');
 const app = express();
 
-const { products } = require('./data.js');
+// app.use([logger, authorize]);
+app.use(morgan('tiny'));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1><a href="/api/products">Products</a>');
+app.get('/', (req,res)=> {
+    res.send('Home');
 });
-
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image };
-    });
-    res.json(newProducts);
+app.get('/about', (req,res)=>{
+    res.send('About');
 });
-
-app.get('/api/products/:productID', (req, res) => {
-    const product = products.find((p) => p.id === Number(req.params["productID"]));
-    if (!product) {
-        return res.status(404).send('Product DNE');
-    }
-    res.json(product);
+app.get('/api/products', (req,res)=>{
+    res.send('Products');
 });
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query);
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
-    if (search) {
-        sortedProducts = sortedProducts.filter((pro) => {
-            return pro.name.startsWith(search);
-        });
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if (sortedProducts.length < 1) {
-        return res.status(200).json({success:true, data:[]});
-    }
-    res.status(200).json(sortedProducts);
-    res.send('Hi!');
+app.get('/api/items', (req,res)=>{
+    res.send('Items');
 });
 
 app.listen(5000, () => {
-    console.log("Server is listening...");
+    console.log("Server is listening on port 5000 ...");
 });
 
