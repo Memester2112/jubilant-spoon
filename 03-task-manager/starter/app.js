772c753add8f5b745a1498ce7f605dc1c674141f
2efb7c0586666e0570786fb9594a1329a46582b6 <aryendrasingh1525@gmail.com>
2022-09-19 20:08:28 +0530

    Database up and running using .env secret variable

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,32 @@
-console.log('Task Manager App')
+const connectDB = require('./db/connect');
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+require('dotenv').config()
+//middleware
+app.use(express.json());
+
+//routes 
+app.get('/hello', (req, res) => {
+    res.send('Task Manager');
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on port ${port}...`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,27 @@
+
+const getAllItems = (req,res) => {
+    res.send('get all tasks');
+}
+const createTask = (req,res) => {
+    res.json(req.body);
+}
+const getTask = (req,res) => {
+    res.json({id: req.params.id});
+}
+const updateTask = (req,res) => {
+    res.send('updated task');
+}
+const deleteTask = (req,res) => {
+    res.send('deleted task');
+}
+
+
+
+
+module.exports = {
+    getAllItems,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
